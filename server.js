a24fe341452f3090cb115906dd4f24d87d6aafe8
exsample016-dotcom <exsample016@gmail.com>
2026-05-19 22:11:58 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON ${PORT}`);
+});
